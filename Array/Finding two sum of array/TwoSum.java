1387e270ca959076ff4f1066e7312fd3ce820454
Esakkidurai <112741698+esakki-durai@users.noreply.github.com>
2023-02-10 00:42:03 -0800

    Add files via upload

---
+++ Array/Finding two sum of array/TwoSum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class TwoSum{
+	public static int sum(int[] arr){
+		System.out.println("Enter the number:");
+		int z=new Scanner(System.in).nextInt();
+		for(int i=0;i<arr.length;i++){
+			for(int j=0;j<arr.length;j++){
+				if(arr[i]+arr[j]==z){
+					System.out.println(arr[i]+" + "+arr[j]+" = "+z);}}}
+		return z;
+	}
+	public static void main(String[] args) {
+		
+	
+		int a[]={1,2,3,4,5,6,7,8,9};
+		int i=0;
+		while(i<4){
+		int b=sum(a);}}}
